3cb1a28efd0a7d7432486e4878717d86036bd17c
shifttag <ehdgus0837@naver.com>
2024-12-20 16:03:19 +0900

    20241220 수정

--- yumyum/src/main/java/org/koreait/yumyum/dto/menu/request/MenuCategoryRequestDto.java
+++ yumyum/src/main/java/org/koreait/yumyum/dto/menu/request/MenuCategoryRequestDto.java
@@ -6,4 +6,6 @@ import lombok.Data;
 public class MenuCategoryRequestDto {
     private Long id;
     private String menuCategory;
+    private int MenuCategorySequence;
 }
+
--- yumyum/src/main/java/org/koreait/yumyum/repository/MenuCategoryRepository.java
+++ yumyum/src/main/java/org/koreait/yumyum/repository/MenuCategoryRepository.java
@@ -4,6 +4,10 @@ import org.koreait.yumyum.entity.MenuCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MenuCategoryRepository extends JpaRepository<MenuCategory, Long> {
+    List<MenuCategory> findByMenuCategorySequenceBetween(int start, int end);
+    List<MenuCategory> findAllByOrderByMenuCategorySequenceAsc();
 }
