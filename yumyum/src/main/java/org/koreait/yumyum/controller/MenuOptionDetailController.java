96298c1fe673004412a866c872cfd57aa6230c49
hiy0ung <hyejee98@gmail.com>
2024-12-30 12:22:27 +0900

    20241230 shy2

--- yumyum/src/main/java/org/koreait/yumyum/controller/MenuOptionDetailController.java
+++ yumyum/src/main/java/org/koreait/yumyum/controller/MenuOptionDetailController.java
@@ -8,6 +8,7 @@ import org.koreait.yumyum.dto.menu.response.MenuOptionDetailResponseDto;
 import org.koreait.yumyum.service.MenuOptionDetailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,26 +19,36 @@ public class MenuOptionDetailController {
     private final MenuOptionDetailService menuOptionDetailService;
 
     public static final String OPTION_DETAIL_POST_ADD = "/add";
-    public static final String OPTION_DETAIL_PUT_ID = "/{id}";
-    public static final String OPTION_DETAIL_DELETE_ID = "/{id}";
+    public static final String OPTION_DETAIL_PUT_ID = "/update/{optionDetailId}";
+    public static final String OPTION_DETAIL_DELETE_ID = "/delete/{optionDetailId}";
 
     @PostMapping(OPTION_DETAIL_POST_ADD)
-    public ResponseEntity<ResponseDto<MenuOptionDetailResponseDto>> addOptionDetail(@RequestBody MenuOptionDetailRequestDto dto) {
-        ResponseDto<MenuOptionDetailResponseDto> response = menuOptionDetailService.addOptionDetail(dto);
+    public ResponseEntity<ResponseDto<MenuOptionDetailResponseDto>> addOptionDetail(
+            @RequestBody MenuOptionDetailRequestDto dto,
+            @AuthenticationPrincipal Long id
+    ) {
+        ResponseDto<MenuOptionDetailResponseDto> response = menuOptionDetailService.addOptionDetail(dto, id);
         HttpStatus status = response.isResult() ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
         return ResponseEntity.status(status).body(response);
     }
 
     @PutMapping(OPTION_DETAIL_PUT_ID)
-    public ResponseEntity<ResponseDto<MenuOptionDetailResponseDto>> updateOptionDetail(@RequestBody MenuOptionDetailRequestDto dto, @PathVariable Long id) {
-        ResponseDto<MenuOptionDetailResponseDto> response = menuOptionDetailService.updateOptionDetail(dto, id);
+    public ResponseEntity<ResponseDto<MenuOptionDetailResponseDto>> updateOptionDetail(
+            @RequestBody MenuOptionDetailRequestDto dto,
+            @PathVariable Long optionDetailId,
+            @AuthenticationPrincipal Long id
+    ) {
+        ResponseDto<MenuOptionDetailResponseDto> response = menuOptionDetailService.updateOptionDetail(dto, optionDetailId, id);
         HttpStatus status = response.isResult() ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
         return ResponseEntity.status(status).body(response);
     }
 
     @DeleteMapping(OPTION_DETAIL_DELETE_ID)
-    public ResponseEntity<ResponseDto<Void>> deleteOptionDetail(@PathVariable Long id) {
-        ResponseDto<Void> response = menuOptionDetailService.deleteOptionDetail(id);
+    public ResponseEntity<ResponseDto<Void>> deleteOptionDetail(
+            @PathVariable Long optionDetailId,
+            @AuthenticationPrincipal Long id
+    ) {
+        ResponseDto<Void> response = menuOptionDetailService.deleteOptionDetail(optionDetailId, id);
         HttpStatus status = response.isResult() ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
         return ResponseEntity.status(status).body(response);
     }
