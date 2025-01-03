e5c6db1db08ef9e84a59fcb29bf527f87597b49d
hiy0ung <hyejee98@gmail.com>
2025-01-03 13:57:46 +0900

    20250103 shy2

--- yumyum/src/main/java/org/koreait/yumyum/controller/MyPageController.java
+++ yumyum/src/main/java/org/koreait/yumyum/controller/MyPageController.java
@@ -8,6 +8,7 @@ import org.koreait.yumyum.dto.user.response.UserResponseDto;
 import org.koreait.yumyum.service.MyPageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,26 +17,26 @@ import org.springframework.web.bind.annotation.*;
 public class MyPageController {
     private final MyPageService myPageService;
 
-    public static final String MYPAGE_GET = "/{id}";
-    public static final String MYPAGE_UPDATE = "/update/{id}";
-    public static final String MYPAGE_DELETE = "/delete/{id}";
+    public static final String MYPAGE_GET = "/";
+    public static final String MYPAGE_UPDATE = "/update";
+    public static final String MYPAGE_DELETE = "/delete";
 
     @GetMapping(MYPAGE_GET)
-    public ResponseEntity<ResponseDto<UserResponseDto>> getAllInfo(@PathVariable Long id) {
+    public ResponseEntity<ResponseDto<UserResponseDto>> getAllInfo(@AuthenticationPrincipal Long id) {
         ResponseDto<UserResponseDto> response = myPageService.getAllInfo(id);
         HttpStatus status = response.isResult() ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
         return ResponseEntity.status(status).body(response);
     }
 
     @PutMapping(MYPAGE_UPDATE)
-    public ResponseEntity<ResponseDto<UserResponseDto>> updateUserInfo(@PathVariable Long id, @RequestBody UserRequestDto dto) {
+    public ResponseEntity<ResponseDto<UserResponseDto>> updateUserInfo(@AuthenticationPrincipal Long id, @RequestBody UserRequestDto dto) {
         ResponseDto<UserResponseDto> response = myPageService.updateUserInfo(id, dto);
         HttpStatus status = response.isResult() ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
         return ResponseEntity.status(status).body(response);
     }
 
     @DeleteMapping(MYPAGE_DELETE)
-    public ResponseEntity<ResponseDto<Void>> deleteUserInfo(@PathVariable Long id) {
+    public ResponseEntity<ResponseDto<Void>> deleteUserInfo(@AuthenticationPrincipal Long id) {
         ResponseDto<Void> response = myPageService.deleteUserInfo(id);
         HttpStatus status = response.isResult() ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
         return ResponseEntity.status(status).body(response);
--- yumyum/src/main/java/org/koreait/yumyum/dto/user/request/UserRequestDto.java
+++ yumyum/src/main/java/org/koreait/yumyum/dto/user/request/UserRequestDto.java
@@ -4,13 +4,8 @@ import lombok.Data;
 
 @Data
 public class UserRequestDto {
-    private Long id;
-    private String userId;
     private String userPw;
-    private String userName;
     private String userEmail;
     private String userPhone;
-    private String userBusinessNumber;
-    private Boolean privacyPolicyAgreed;
     private Boolean marketingAgreed;
 }
