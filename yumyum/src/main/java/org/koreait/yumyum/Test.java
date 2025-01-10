4a9c94ee57630128436a0b61b4d57caf563f8b8e
shifttag <ehdgus0837@naver.com>
2025-01-10 09:46:02 +0900

    20250110 hdh

---
+++ yumyum/src/main/java/org/koreait/yumyum/Test.java
@@ -0,0 +1,16 @@
+package org.koreait.yumyum;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+
+public class Test implements CommandLineRunner {
+
+    @Value("${user.dir}")
+    private String path;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(path + "/upload");
+    }
+}
+
