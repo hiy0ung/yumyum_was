9e49455d6a2a63ed3fb0becd17a0cd32ee59fe8a
shifttag <ehdgus0837@naver.com>
2025-01-11 23:45:31 +0900

    20250111 hdh

--- yumyum/src/main/java/org/koreait/yumyum/service/MenuOptionDetailService.java
+++ yumyum/src/main/java/org/koreait/yumyum/service/MenuOptionDetailService.java
@@ -2,6 +2,7 @@ package org.koreait.yumyum.service;
 
 import org.koreait.yumyum.dto.ResponseDto;
 import org.koreait.yumyum.dto.menu.request.MenuOptionDetailRequestDto;
+import org.koreait.yumyum.dto.menu.request.MenuOptionDetailUpdateRequestDto;
 import org.koreait.yumyum.dto.menu.response.MenuOptionDetailResponseDto;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 public interface MenuOptionDetailService {
     ResponseDto<MenuOptionDetailResponseDto> addOptionDetail(MenuOptionDetailRequestDto dto, Long id);
 
-    ResponseDto<MenuOptionDetailResponseDto> updateOptionDetail(MenuOptionDetailRequestDto dto, Long optionDetailId, Long id);
+    ResponseDto<MenuOptionDetailResponseDto> updateOptionDetail(MenuOptionDetailUpdateRequestDto dto, Long optionDetailId, Long id);
 
 
     ResponseDto<Void> deleteOptionDetail(Long optionDetailId, Long id);
